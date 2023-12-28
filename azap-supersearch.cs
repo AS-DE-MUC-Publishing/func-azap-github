0178bf14117d5810e3e01529662d5c18025ebf67
Marc Simonis <marc.simonis@bertelsmann.de>
2023-12-28 10:02:15 +0100

    update

--- azap-supersearch.cs
+++ azap-supersearch.cs
@@ -31,6 +31,7 @@ namespace azap
             string user = data?.user;  
             string authentication=data?.authentication;
             string environment=data?.environment;
+            string query=data?.query;
 
             string connectionString;
             SqlConnection connection;
@@ -45,13 +46,13 @@ namespace azap
                     connectionString=$"Server=tcp:{servername},1433;Initial Catalog={database};";  
                     if (servername.Contains("azuresynapse")) {
                         url_token="https://sql.azuresynapse.net/.default";
-                        mylog.LogInformation("url_token from: " + authentication);
+                       // mylog.LogInformation("url_token from: " + authentication);
                     }
                      else {
                         url_token="https://database.windows.net/.default";             
                     }                  
                     var token = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { url_token }));
-                    mylog.LogInformation("token: " + token);
+                  //  mylog.LogInformation("token: " + token);
                     connection = new SqlConnection(connectionString);
                     connection.AccessToken = token.Token;                     
                 }
@@ -69,7 +70,7 @@ namespace azap
                 var secretClient = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
                 KeyVaultSecret secret = await secretClient.GetSecretAsync("pw-supersearch");
                 connectionString = $"Server=tcp:{servername},1433;Initial Catalog={database};User ID={user};Password={secret.Value};";
-                mylog.LogInformation("SQL-User-Authentification: " + authentication);
+             //   mylog.LogInformation("SQL-User-Authentification: " + authentication);
                 connection = new SqlConnection(connectionString);    
             }
 
@@ -77,7 +78,7 @@ namespace azap
 
             await connection.OpenAsync();
             // SQL Query
-            string query = "SELECT COUNT(*) FROM search.cubis_product";
+            // string query = "SELECT COUNT(*) FROM search.cubis_product";
             using (SqlCommand command = new SqlCommand(query, connection))
             {
                 rowCount = (int)await command.ExecuteScalarAsync();
@@ -86,49 +87,6 @@ namespace azap
             // If no rows were found, return a default value
             return new OkObjectResult(rowCount.ToString());
         }
-    }
-
-        // logging disabled
-    // public static class azap_audit // logging disabled
-    // {
-    // public static string Auditlogger(String logname, String functionname, String load_id, String pipeline, String infostr, String runtime)    {
-
-    //         string connectionString = Environment.GetEnvironmentVariable("auditlogConnection");  
-    //         String result="";
-    //         var query=$"";
-    //         // Using the connection string to open a connection
-    //         try{
-    //             using(SqlConnection connection = new SqlConnection(connectionString)){
-    //                 // Opening a connection
-    //                 connection.Open(); 
-    //                 switch (logname)     
-    //                     {
-    //                         case "log_azurefunction":
-    //                              query = $"EXEC [audit].[usp_log_azurefunction_add] @pipeline = N'{pipeline}',@load_id ={load_id},@azurefunction = N'{functionname}',@infostr = N'{infostr}',@runtime ='{runtime}'";
-    //                             break;    
-    //                         case "log_blobcopy":
-    //                              query = $"EXEC [audit].[usp_log_blobcopy_add] @pipeline = N'{pipeline}',@load_id ={load_id},@azurefunction = N'{functionname}',@blobname = N'{infostr}',@runtime ='{runtime}'";
-    //                             break;             
-    //                         default:
-    //                           query = $"Select 0";
-    //                         break;
-    //                     }
-                   
-    //                 SqlCommand command = new SqlCommand(query,connection); 
-    //                 // Open the connection, execute and close connection
-    //                 if(command.Connection.State == System.Data.ConnectionState.Open){
-    //                     command.Connection.Close();
-    //                 }
-    //                 command.Connection.Open();
-    //                 command.ExecuteNonQuery(); 
-    //                 result="Success";
-    //             }
-    //         }
-    //         catch(Exception e){               
-    //             result = e.ToString();
-    //         }
-    //     return result;
-    //     }
-    // }
+    } 
 
 }
